f6b71074922b821e219e9d36881a7ac3aecf5888
mateo316 <mateoaguilar123@gmail.com>
2025-03-01 00:06:00 -0500

    complete migration to php
    login page, complete migration to php, no html files left :)

--- g1.php
+++ g1.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+<script src="yes.js"> </script>
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <meta charset="UTF-8">
@@ -14,15 +15,15 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
   <header>
-    <a class="nav-link active" href="index.html"><h1>AILAN PAGE IS LIVE NOW!!!</h1></a>
-    <a class="announcement" href="a1.html">CLICK HERE</a>
+    <a class="nav-link active" href="index.php"><h1 class="text-center">AILAN PAGE IS LIVE NOW!!!</h1></a>
+    <a class="announcement" href="a1.php">CLICK HERE</a>
   </header>
 
   <nav class="navbar navbar-expand-lg bg-terciary">
 
     <div class="container-fluid">
 
-      <a class="navbar-brand me-auto" href="index.html">
+      <a class="navbar-brand me-auto" href="index.php">
         <img src="images/5.jpeg" class="rounded-circle  me-2" alt="Bootstrap" width="60" height="60">
       </a>
 
@@ -32,16 +33,17 @@
 
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav border-mobile">
-          <a class="nav-link disabled mx-2" href="g1.html">Demacrado</a>
-          <a class="nav-link active mx-2" href="a1.html">Ailan</a>
-          <a class="nav-link active mx-2" href="thanks.html">Thanks</a>
+          <a class="nav-link disabled mx-2" href="g1.php">Demacrado</a>
+          <a class="nav-link active mx-2" href="a1.php">Ailan</a>
+          <a class="nav-link active mx-2" href="thanks.php">Thanks</a>
         </div>
+        <button type="button" class="btn btn-danger m-2" onclick="logout()">Logout</button>
       </div>
     </div>
 
   </nav>
 
-  <h2 class="announcement">Coming Soon...</h2>
+  <h2 class="text-center">Coming Soon...</h2>
   <div id="pic"><img src="images/cartman.jpeg" class="mx-auto d-block img-fluid"></div>
 
   <footer class="mt-auto">© 2025, mateo316</footer>
--- lockedPage.php
+++ lockedPage.php
@@ -1,5 +1,13 @@
+<?php
+session_start();
+
+if (isset($_SESSION["valid"]) && $_SESSION["valid"]) {
+	// Page with Secrets
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+<script src="yes.js"> </script>
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <meta charset="UTF-8">
@@ -23,12 +31,12 @@
 <body class="d-flex flex-column min-vh-100">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <header>
-        <a class="nav-link active" href="index.html"><h1>AILAN PAGE IS LIVE NOW!!!</h1></a>
-        <a class="announcement" href="a1.html">CLICK HERE</a>
+        <a class="nav-link active" href="index.php"><h1 class="text-center">AILAN PAGE IS LIVE NOW!!!</h1></a>
+        <a class="announcement" href="a1.php">CLICK HERE</a>
     </header>
     <nav class="navbar navbar-expand-lg bg-terciary">
       <div class="container-fluid">
-        <a class="navbar-brand me-auto" href="index.html">
+        <a class="navbar-brand me-auto" href="index.php">
           <img src="images/5.jpeg" class="rounded-circle me-2" alt="Bootstrap" width="60" height="60">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -36,10 +44,11 @@
       </button> 
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="navbar-nav border-mobile">
-            <a class="nav-link active mx-2" href="g1.html">Demacrado</a>
-            <a class="nav-link active mx-2" href="a1.html">Ailan</a>
-            <a class="nav-link active mx-2" href="thanks.html">Thanks</a>
+            <a class="nav-link active mx-2" href="g1.php">Demacrado</a>
+            <a class="nav-link active mx-2" href="a1.php">Ailan</a>
+            <a class="nav-link active mx-2" href="thanks.php">Thanks</a>
           </div>
+		  <button type="button" class="btn btn-danger m-2" onclick="logout()">Logout</button>
         </div>
       </div>
   </nav>
@@ -81,6 +90,22 @@
             </button>
           </div>
     </main>
+
     <footer class="mt-auto">© 2025, mateo316</footer>
 </body>
+
 </html>
+
+<?php } else { 
+	// Page with no secrets!
+?>
+	
+<html>
+
+Nope! Nope! Nope! All the Nope!
+<br>
+<h1 class="text-center">BEGONE!</h1>
+
+</html>
+
+<?php } ?>
